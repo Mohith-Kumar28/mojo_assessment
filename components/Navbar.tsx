f0355b1bf8bd70227ab289a0396e16e4675cd558
Mohith-Kumar28 <mohithkumar808@gmail.com>
2024-06-30 16:01:52 +0530

    removed hamburger

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -85,16 +85,6 @@ const Navbar = () => {
               </Button>
             )}
           </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              onClick={() => setMobileMenuOpen(true)}
-            >
-              <span className="sr-only">Open main menu</span>
-              <FaBars className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
         </nav>
       </header>
     </>
