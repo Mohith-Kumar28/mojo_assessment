9339b029af661d5fec167affdba047f42f571aa3
Mohith-Kumar28 <mohithkumar808@gmail.com>
2024-06-30 15:56:11 +0530

    added signout btn

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
       <body className={inter.className}>
         <SessionProvider session={session}>
           <div className="mx-auto max-w-5xl text-2xl gap-2 mb-10">
-            {/* <Navbar /> */}
+            <Navbar />
             <Providers>{children}</Providers>
           </div>
         </SessionProvider>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,6 +11,8 @@ import Image from "next/image";
 
 import ProfileDropdown from "@/app/profile/profileDropdown";
 import PagesList from "./pages-list";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
 
 const Navbar = () => {
   const { data: session }: any = useSession();
@@ -70,42 +72,17 @@ const Navbar = () => {
           className="mx-auto flex max-w-7xl items-center justify-between gap-x-6 p-6 lg:px-8"
           aria-label="Global"
         >
-          {/* <div className="flex lg:flex-1">
-            <Link href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <Image src="/logo 1.png" width={50} height={50} alt="star logo" />
-            </Link>
-          </div>
-          <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <Link
-                key={item.name}
-                href={item.href}
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                {item.name}
-              </Link>
-            ))}
-          </div> */}
           <div className="flex flex-1 items-center justify-end gap-x-6">
-            {!session ? (
-              <>
-                <button
-                  onClick={() => {
-                    signIn("facebook");
-                  }}
-                  className="flex items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-1.5 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]"
-                >
-                  <FaFacebook />
-                  <span className="text-sm font-semibold leading-6">
-                    Login with Facebook
-                  </span>
-                </button>
-              </>
-            ) : (
-              <>
-                <ProfileDropdown />
-              </>
+            {!session && (
+              <Button
+                variant="ghost"
+                onClick={() => {
+                  signOut();
+                }}
+              >
+                Log out
+                <LogOut className="size-4 ml-2" />
+              </Button>
             )}
           </div>
           <div className="flex lg:hidden">
@@ -119,68 +96,6 @@ const Navbar = () => {
             </button>
           </div>
         </nav>
-        <Dialog
-          as="div"
-          className="lg:hidden"
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}
-        >
-          <div className="fixed inset-0 z-10" />
-          <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center gap-x-6">
-              <Link href="/" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <Image
-                  width={50}
-                  height={50}
-                  src="/logo 1.png"
-                  alt="star logo mobile"
-                />
-              </Link>
-              {session ? (
-                <button
-                  onClick={() => {
-                    signOut();
-                  }}
-                  className="ml-auto rounded-md bg-black border border-1 border-gray-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Log out
-                </button>
-              ) : (
-                <Link
-                  href="/register"
-                  className="ml-auto rounded-md bg-black border border-1 border-gray-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign up
-                </Link>
-              )}
-
-              <button
-                type="button"
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-                onClick={() => setMobileMenuOpen(false)}
-              >
-                <span className="sr-only">Close menu</span>
-                <FaXmark className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </Dialog.Panel>
-        </Dialog>
       </header>
     </>
   );
